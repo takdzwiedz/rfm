8e9e002bd1fa6a589df1867ff271d44f78ba1315
takdzwiedz <artur.kacprzak@itcenter.pl>
2019-10-07 14:00:32 +0100

    Wyswietlanie tabeli z zamowieniami

--- src/Controller/AnalysisController.php
+++ src/Controller/AnalysisController.php
@@ -1,16 +1,27 @@
 <?php
 
-
 namespace App\Controller;
 
-
 use App\Entity\Artykuly;
+use Doctrine\DBAL\Driver\Connection;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AnalysisController extends AbstractController
 {
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    public function __construct(
+        Connection $connection)
+    {
+
+        $this->connection = $connection;
+    }
+
     /**
      * @Route("/")
      */
@@ -36,9 +47,29 @@ class AnalysisController extends AbstractController
             );
         }
 
-//        return new Response('Oto artykuł: '.$article->getNazwaArtykulu());
         return $this->render('analysis/article.html.twig', [
             'article_name' => $article->getNazwaArtykulu()
         ]);
     }
+
+    /**
+     * @Route("/order", name="article_all")
+     * @return Response
+     */
+    public function order()
+    {
+        $orders = $this->connection->fetchAll(
+            'SELECT *
+                FROM zamowienia
+                LEFT JOIN kontrahenci k on zamowienia.id_kontrahenta = k.id_kontrahenta
+                ORDER BY wartosc_netto DESC
+                LIMIT 25');
+
+        return $this->render(
+            'analysis/order.html.twig', [
+                'orders' => $orders
+            ]
+        );
+    }
 }
+

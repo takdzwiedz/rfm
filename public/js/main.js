4a4e4fa02eee47eff956913a9b68c8d5c6cd7cda
Artur Kacprzak <artur.kacprzak@itcenter.pl>
2019-11-05 13:54:39 +0100

    fix

--- public/js/main.js
+++ public/js/main.js
@@ -12,74 +12,41 @@ $(document).ready(function () {
 
 
     let r = Math.floor(Math.random() * 11);
-
-
-
-    // $("#btn2").click(function(){
-    //     $("#art").append("<a class=\"a-name\" href=\"#\" data-toggle=\"collapse\" >Grupa 1</a>\n" +
-    //         "                        <a href=\"#homeSubmenu" + r + 1 +"\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
-    //         "                           class=\"a-arrow dropdown-toggle\"></a>\n" +
-    //         "\n" +
-    //         "                        <ul class=\"collapse list-unstyled\" id=\"homeSubmenu" + r + 1 +"\">\n" +
-    //         "                            <li class=\"\">\n" +
-    //         "                                <a href=\"#\" class=\"a-name\">Kontrahent 1</a>\n" +
-    //         "                                <a href=\"#homeSubmenu3\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
-    //         "                                   class=\"a-arrow dropdown-toggle\"></a>\n" +
-    //         "                                <ul class=\"collapse list-unstyled\" id=\"homeSubmenu3\">\n" +
-    //         "                                    <li>\n" +
-    //         "                                        <a href=\"#\">Klient 1</a>\n" +
-    //         "                                    </li>\n" +
-    //         "                                    <li>\n" +
-    //         "                                        <a href=\"#\">Klient 2</a>\n" +
-    //         "                                    </li>\n" +
-    //         "                                    <li>\n" +
-    //         "                                        <a href=\"#\">Klient 3</a>\n" +
-    //         "                                    </li>\n" +
-    //         "                                </ul>\n" +
-    //         "                            </li>\n" +
-    //         "                            <li>\n" +
-    //         "                                <a href=\"#\">Kontrahent 2</a>\n" +
-    //         "                            </li>\n" +
-    //         "                            <li>\n" +
-    //         "                                <a href=\"#\">Kontrahent 3</a>\n" +
-    //         "                            </li>\n" +
-    //         "                        </ul>");
-    // });
 });
 
-function getAllClientGroups(){
+function getAllClientGroups() {
 
     $.ajax({
         url: 'groups2',
         method: 'post',
-        success: function(res){
+        success: function (res) {
             Object.keys(res).forEach(key => {
-                $("#art").append("<a class=\"a-name\" href=\"#\" data-toggle=\"collapse\" >" + res[key]['nazwa_grupy'] +"</a>\n" +
-                    "                        <a href=\"#homeSubmenu" + key +"\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
-                    "                           class=\"a-arrow dropdown-toggle\" id=\""+ res[key]['id'] +"\"></a>\n" +
+                $("#art").append("<a class=\"a-name\" href=\"#\" data-toggle=\"collapse\" id=\"" + res[key]['id'] + " \" >" + res[key]['nazwa_grupy'] + "</a>\n" +
+                    "                        <a href=\"#homeSubmenu" + key + "\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
+                    "                           class=\"a-arrow dropdown-toggle\" id=\"" + res[key]['id'] + "\" onclick=\"getAllClient(" + res[key]['id'] + ")\"></a>\n" +
                     "\n" +
-                    "                        <ul class=\"collapse list-unstyled\" id=\"homeSubmenu" + key +"\">\n" +
+                    "                        <ul class=\"collapse list-unstyled\" id=\"homeSubmenu" + key + "\">\n" +
                     "                            <li class=\"\">\n" +
                     "                                <a href=\"#\" class=\"a-name\">Kontrahent 1</a>\n" +
-                    "                                <a href=\"#homeSubmenu3" + key +"\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
+                    "                                <a href=\"#homeSubmenu3" + key + "\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
                     "                                   class=\"a-arrow dropdown-toggle\"></a>\n" +
-                    "                                <ul class=\"collapse list-unstyled\" id=\"homeSubmenu3" + key +"\">\n" +
-                        "                            <li class=\"\">\n" +
-                        "                                <a href=\"#\" class=\"a-name\">Użytkownik 1</a>\n" +
-                        "                                <a href=\"#homeSubmenu4" + key +"\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
-                        "                                   class=\"a-arrow dropdown-toggle\"></a>\n" +
-                        "                                <ul class=\"collapse list-unstyled\" id=\"homeSubmenu4" + key +"\">\n" +
-                        "                                    <li>\n" +
-                        "                                        <a href=\"#\">Zamówienie 1</a>\n" +
-                        "                                    </li>\n" +
-                        "                                    <li>\n" +
-                        "                                        <a href=\"#\">Zamówienie 2</a>\n" +
-                        "                                    </li>\n" +
-                        "                                    <li>\n" +
-                        "                                        <a href=\"#\">Zamówienie 3</a>\n" +
-                        "                                    </li>\n" +
-                        "                                </ul>\n" +
-                        "                            </li>\n" +
+                    "                                <ul class=\"collapse list-unstyled\" id=\"homeSubmenu3" + key + "\">\n" +
+                    "                            <li class=\"\">\n" +
+                    "                                <a href=\"#\" class=\"a-name\">Użytkownik 1</a>\n" +
+                    "                                <a href=\"#homeSubmenu4" + key + "\" data-toggle=\"collapse\" aria-expanded=\"false\"\n" +
+                    "                                   class=\"a-arrow dropdown-toggle\"></a>\n" +
+                    "                                <ul class=\"collapse list-unstyled\" id=\"homeSubmenu4" + key + "\">\n" +
+                    "                                    <li>\n" +
+                    "                                        <a href=\"#\">Zamówienie 1</a>\n" +
+                    "                                    </li>\n" +
+                    "                                    <li>\n" +
+                    "                                        <a href=\"#\">Zamówienie 2</a>\n" +
+                    "                                    </li>\n" +
+                    "                                    <li>\n" +
+                    "                                        <a href=\"#\">Zamówienie 3</a>\n" +
+                    "                                    </li>\n" +
+                    "                                </ul>\n" +
+                    "                            </li>\n" +
                     "                                    <li>\n" +
                     "                                        <a href=\"#\">Użytkownik 2</a>\n" +
                     "                                    </li>\n" +
@@ -97,8 +64,12 @@ function getAllClientGroups(){
                     "</li>");
             });
         },
-        error: function(){
+        error: function () {
             console.log('failure');
         },
     })
+}
+
+function getAllClient() {
+
 }
